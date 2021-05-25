49c96d3a2aeb520be5e77aac3ceb88b0eff81101
camitobon <82970265+camitobon@users.noreply.github.com>
2021-05-25 14:10:00 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,52 @@
+namespace SpriteKind {
+    export const door = SpriteKind.create()
+}
+function level_two () {
+    tiles.setTilemap(tilemap`level2`)
+    enemyreal = sprites.create(assets.image`enemyreallol`, SpriteKind.Enemy)
+    enemyreal.follow(mySprite, 30)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.door, function (sprite, otherSprite) {
+    level_two()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    statusbar.value += -1
+    if (statusbar.value == 0) {
+        game.over(false)
+    }
+})
+let statusbar: StatusBarSprite = null
+let enemyreal: Sprite = null
+let mySprite: Sprite = null
 tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+scene.cameraFollowSprite(mySprite)
+enemyreal = sprites.create(assets.image`enemyreallol`, SpriteKind.Enemy)
+enemyreal.setPosition(24, 88)
+statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.value = 100
+statusbar.setPosition(17, 6)
+enemyreal = sprites.create(assets.image`enemyreallol`, SpriteKind.Enemy)
+enemyreal.setPosition(160, 39)
+enemyreal = sprites.create(assets.image`enemyreallol`, SpriteKind.Enemy)
+enemyreal.setPosition(186, 231)
+let mySprite2 = sprites.create(assets.image`doorway`, SpriteKind.door)
+mySprite2.setPosition(240, 200)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,11 +2,13 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100001010103040404020d0d0e0d0e0d0c0c010101050c0c0c050d0d0d0d0d0d0c0c010101050c0c0c0b04060d0304020c0c010101050c0c0c050d0d0d050c050c0e010101050c0c0c050d0d0d050c050c0c0c0c0c050c0c0c050d0d0d050c050c0c0e0a0e050c0c0c050d0d0d050c050c0e0e050e050c0c0c080a0a0a070c050c0c0e050e050c0c0c0e050c0c0c0c050c0e0e050e050c0304040904020c0c050c0c030904070c050d0d0d0d050c0c050c0c050c0c0c0c050d0d0d0d050c0c050c0c050c0c0c0c050d0d0d0d050c0c08060c0804040404070d0d0d0d050c0c0c0c0c01010d01010101010101080c0e0c0c0c01010d0101010101010101010101010c`, img`
+            case "level1":return tiles.createTilemap(hex`1000100001010103040404020d0d0e0d0e0d0c0c010101050c0c0c050d0d0d0d0d0d0c0c010101050c0c0c0b04060c0304020c0c010101050c0c0c050d0d0d050c050c0e010101050c0c0c050d0d0d050c050c0c0c0c0c050c0c0c050d0d0d050c050c0c0e0a0e050c0c0c050c0d0d050c050c0e0e050e050c0c0c080a0a0a070c050c0c0e050e050c0c0c0e050c0c0c0c050c0e0e050e050c0304040904020c0c050c0c030904070c050d0d0d0d050c0c050c0c050c0c0c0c050d0d0d0d050c0c050c0c050c0c0c0c050d0d0d0d050c0c08060c0804040404070d0d0d0d050c0c0c0c0c01010d01010101010101080c010c0c0c01010d0101010101010101010101010c`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -24,6 +26,25 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,sprites.castle.tileDarkGrass1,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn1,sprites.vehicle.roadHorizontal,sprites.vehicle.roadVertical,sprites.vehicle.roadIntersection4,sprites.vehicle.roadTurn4,sprites.vehicle.roadTurn3,sprites.vehicle.roadIntersection1,sprites.vehicle.roadIntersection3,sprites.vehicle.roadIntersection2,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass2,sprites.builtin.forestTiles0], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`10001000040303030303030303030303030303050902020202020a0a0a0a0202020202070902020202020202020202020202020709020202020202020202020a0a0a02070902020a0a0a020202020202020202070902020202020202020202020202020709020202020202020a0a0a0a02020207090a0a0a0a0a020202020202020202070902020202020202020202020a0a0a0b090202020202020202020202020202070902020202020a0a0a02020202020207090202020202020202020a0a0a0a0207090a0a0a0a020202020202020202020709020202020202020a0a0a02020202070902020202020202020202020202020701080808080808080808080808080806`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundWest,sprites.dungeon.collectibleInsignia,sprites.dungeon.collectibleBlueCrystal], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +52,8 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "this is actually enemy":
+            case "tile1":return tile1;
         }
         return null;
     })
